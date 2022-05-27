3c116ea8e541df96f6e382deb96ac5ff355227b6
solomanray <96454998+solomanray@users.noreply.github.com>
2022-05-27 12:59:28 +0800

    Guessing Game ---13th commit

--- src/main/java/com/ray/ui/GuessFrame.java
+++ src/main/java/com/ray/ui/GuessFrame.java
@@ -4,27 +4,44 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame extends JFrame {
 
     public GuessFrame(){
         super();
+        Random random = new Random();
+        int secret = random.nextInt(100)+1;
+        JLabel jLabel = new JLabel("1~100");
+        JButton button =new JButton("GUESS");
+        JTextField number = new JTextField(10);
+
         setSize(650,400);
         setLocation(444,222);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        JButton button =new JButton("Start");
-        JLabel jLabel = new JLabel("New Game");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                System.out.println("Game Start!");
-                jLabel.setText("Good Luck!");
-            }
+
+                int num = Integer.parseInt(number.getText());
+                System.out.println(num);
+
+                if (num>secret){
+                        jLabel.setText("Smaller");
+                    }else if (num < secret){
+                        jLabel.setText("Bigger");
+                    }else {
+                    System.out.println("You win!!");
+                    jLabel.setText("Bingo!!");
+                }
+                }
         });
 
         setLayout(new FlowLayout());
-        add(jLabel);
+        add(number);
         add(button);
+        add(jLabel);
+
 
         setVisible(true);
     }
